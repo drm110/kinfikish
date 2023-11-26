6ff086488fe7e2abbb6011bbe5c83f75557c99ff
Mian Ishtiaq <35779127+devbyishtiaq@users.noreply.github.com>
2023-11-27 00:46:33 +0500

    removed collection section

--- pages/index.js
+++ pages/index.js
@@ -34,7 +34,6 @@ export default function Home() {
     <NewTshirts/>
     <Productsbadgecustomizer/>
     <Customizae/>
-    <Collections/>  
     <Followinstagram/>
     <Followtiktok/>
     <Subscribekinki/>
