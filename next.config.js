1f2ae123a3f04f526a6a1f8581549cfa07a7f3cd
Mian Ishtiaq <35779127+devbyishtiaq@users.noreply.github.com>
2023-12-31 17:47:41 +0500

    testing

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const allowedImageWordPressDomain = new URL( process.env.NEXT_PUBLIC_WORDPRESS_SITE_URL ).hostname;
-const nextConfig = {
-    images: {
-		domains: [ allowedImageWordPressDomain, 'via.placeholder.com', 'secure.gravatar.com' ],
-	},
-}
-
-module.exports = nextConfig
